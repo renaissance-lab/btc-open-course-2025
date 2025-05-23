884596ced577c1658bcf6feb35a1e34b6e1c2d38
luckychacha <luckychachaa@gmail.com>
2025-05-23 21:28:05 +0800

    test: segwit+legacy

--- course_02/homework/luckychacha/test.py
+++ course_02/homework/luckychacha/test.py
@@ -1,125 +1,144 @@
-# Copyright (C) 2018-2025 The python-bitcoin-utils developers
-#
-# This file is part of python-bitcoin-utils
-#
-# It is subject to the license terms in the LICENSE file found in the top-level
-# directory of this distribution.
-#
-# No part of python-bitcoin-utils, including this file, may be copied,
-# modified, propagated, or distributed except according to the terms contained
-# in the LICENSE file.
-
+# 多种输入类型（legacy、segwit、taproot）到legacy地址的转账代码
 from bitcoinutils.setup import setup
 from bitcoinutils.utils import to_satoshis
 from bitcoinutils.transactions import Transaction, TxInput, TxOutput, TxWitnessInput
-from bitcoinutils.keys import P2pkhAddress, PrivateKey
+from bitcoinutils.keys import PrivateKey, P2pkhAddress
 from bitcoinutils.script import Script
 
-
 def main():
-    # always remember to setup the network
-    setup("testnet")
-
-    # the key that corresponds to the P2WPKH address
-    priv1 = PrivateKey("cMwQGKT54nzG2XesGda8rSPDhLuVWNr7CCbZk5UvkiBfKEdkAqwh")
-    priv2 = PrivateKey("cMwQGKT54nzG2XesGda8rSPDhLuVWNr7CCbZk5UvkiBfKEdkAqwh")
-    priv3 = PrivateKey("cMwQGKT54nzG2XesGda8rSPDhLuVWNr7CCbZk5UvkiBfKEdkAqwh")
-
-    pub1 = priv1.get_public_key()
-    pub2 = priv2.get_public_key()
-    pub3 = priv3.get_public_key()
-
-    fromAddress1 = pub1.get_address()
-    fromAddress2 = pub2.get_segwit_address()
-    fromAddress3 = pub3.get_taproot_address()
-    print(fromAddress1.to_string())
-    print(fromAddress2.to_string())
-    print(fromAddress3.to_string())
-
-    # UTXO of fromAddress's respectively
-    txid1 = "9074b92dfd270ba4eb928bb7ea03ce69a80ef538bd3d354761411b4aa35c91be"
-    vout1 = 0
-    txid2 = "9074b92dfd270ba4eb928bb7ea03ce69a80ef538bd3d354761411b4aa35c91be"
-    vout2 = 1
-    txid3 = "1a338c80a5be61258ea4e5b5f1ce2d213c22fa340afdd1999a90135e775022bb"
-    vout3 = 0
-
-    # all amounts are needed to sign a taproot input
-    # (depending on sighash)
-    amount1 = to_satoshis(0.00014800)
-    amount2 = to_satoshis(0.00014800)
-    amount3 = to_satoshis(0.00001)
-    amounts = [amount1, amount2, amount3]
+    # 设置测试网
+    setup('testnet')
+    
+    # 私钥
+    from_private_key = PrivateKey('cMwQGKT54nzG2XesGda8rSPDhLuVWNr7CCbZk5UvkiBfKEdkAqwh')
+    from_pub = from_private_key.get_public_key()
+    
+    # 从同一私钥生成3种不同类型的地址
+    legacy_address = from_pub.get_address()
+    segwit_address = from_pub.get_segwit_address()
+    taproot_address = from_pub.get_taproot_address()
+    
+    # 接收方地址（Legacy）
+    to_address = P2pkhAddress('mfkSgFzcijcub1AXuvLMhHbXY5Wodyy5z1')
+    
+    # 显示各种地址信息
+    print("\n=== 地址信息 ===")
+    print(f"私钥 WIF: {from_private_key.to_wif()}")
+    print(f"Legacy 地址: {legacy_address.to_string()}")
+    print(f"SegWit 地址: {segwit_address.to_string()}")
+    print(f"Taproot 地址: {taproot_address.to_string()}")
+    print(f"目标 Legacy 地址: {to_address.to_string()}")
+    
+    
+    legacy_amount = to_satoshis(0.00014800)
+    segwit_amount = to_satoshis(0.00014800)
+    taproot_amount = to_satoshis(0.00001)
+    
     # 计算总输入金额
-    total_input = amount1 + amount2 + amount3
+    total_input = legacy_amount + taproot_amount
     
     # 设置手续费
     fee = to_satoshis(0.000004)  # 0.000004 BTC
+    # fee = to_satoshis(estimate_fee())
+    
     # 计算发送金额
     amount_to_send = total_input - fee
 
-    # all scriptPubKeys are needed to sign a taproot input
-    # (depending on sighash) but always of the spend input
-    script_pubkey1 = fromAddress1.to_script_pub_key()
-    script_pubkey2 = pub2.get_address().to_script_pub_key()
-    script_pubkey3 = fromAddress3.to_script_pub_key()
-    utxos_script_pubkeys = [script_pubkey1, script_pubkey2, script_pubkey3]
+    # 创建多个交易输入
 
-    toAddress = P2pkhAddress('mfkSgFzcijcub1AXuvLMhHbXY5Wodyy5z1')
+    
+    txin_segwit = TxInput(
+        '9074b92dfd270ba4eb928bb7ea03ce69a80ef538bd3d354761411b4aa35c91be',
+        1
+    )
+    
+    txin_taproot = TxInput(
+        '1a338c80a5be61258ea4e5b5f1ce2d213c22fa340afdd1999a90135e775022bb',
+        0
+    )
 
-    # create transaction input from tx id of UTXO
-    txin1 = TxInput(txid1, vout1)
-    txin2 = TxInput(txid2, vout2)
-    txin3 = TxInput(txid3, vout3)
+    txin_legacy = TxInput(
+        '9074b92dfd270ba4eb928bb7ea03ce69a80ef538bd3d354761411b4aa35c91be',
+        0
+    )
+    txinputs = [txin_segwit, txin_legacy]
 
-    # create transaction output
-    txOut = TxOutput(amount_to_send, toAddress.to_script_pub_key())
-
-    # create transaction without change output - if at least a single input is
-    # segwit we need to set has_segwit=True
-    tx = Transaction([txin1, txin2, txin3], [txOut], has_segwit=True)
-
-    print("\nRaw transaction:\n" + tx.serialize())
-
-    print("\ntxid: " + tx.get_txid())
-    print("\ntxwid: " + tx.get_wtxid())
-
-    # sign taproot input
-    # to create the digest message to sign in taproot we need to
-    # pass all the utxos' scriptPubKeys and their amounts
-
+    # 创建交易输出
+    txout = TxOutput(amount_to_send, to_address.to_script_pub_key())
+    
+    # 创建交易
+    tx = Transaction(txinputs, [txout], has_segwit=True)
+    
+    print("\n未签名的交易:")
+    print(tx.serialize())
+    
+    # 准备所有脚本和金额数组（按照新脚本方式）
+    legacy_script = legacy_address.to_script_pub_key()
+    # 从公钥派生脚本，因为地址是segwit地址
+    segwit_script = from_pub.get_address().to_script_pub_key()
+    taproot_script = taproot_address.to_script_pub_key()
+    utxos_script_pubkeys = [segwit_script, legacy_script]
+    amounts = [segwit_amount, legacy_amount]
+    
     # 签名所有输入
-    # sig1 = from_private_key.sign_input(tx, 0, legacy_script)
-    # sig2 = from_private_key.sign_segwit_input(tx, 1, segwit_script, utxos_script_pubkeys[1])
-    # sig3 = from_private_key.sign_taproot_input(tx, 2, utxos_script_pubkeys, amounts)
+    sig1 = from_private_key.sign_segwit_input(tx, 0, segwit_script, amounts[0])
+    # sig3 = from_private_key.sign_taproot_input(tx, 2, [taproot_script], [taproot_amount])
+    # sig2 = from_private_key.sign_taproot_input(tx, 0, [taproot_script], [taproot_amount])
+    sig3 = from_private_key.sign_input(tx, 1, legacy_script)
+    
+    
+    # SegWit输入使用空script_sig和witness
+    txinputs[0].script_sig = Script([])
+
+    # Legacy输入使用script_sig
+    txinputs[1].script_sig = Script([sig3, from_pub.to_hex()])
+    # txinputs[0].script_sig = Script([])
+    
+    # 设置见证数据（只为SegWit和Taproot设置）
+    tx.witnesses = []  # 清空现有见证数据
+    # tx.set_witness(0, TxWitnessInput([sig1, from_pub.to_hex()]))
+    # tx.set_witness(1, TxWitnessInput([sig2]))
+    # tx.witnesses.append(TxWitnessInput([sig1, from_pub.to_hex()]))
+    # tx.witnesses.append(TxWitnessInput([sig2]))
+    tx.set_witness(0, TxWitnessInput([sig1, from_pub.to_hex()]))
+    
+    # 获取签名后的交易
+    signed_tx = tx.serialize()
+    
+    print("\n已签名的交易:")
+    print(signed_tx)
+    
+    print("\n交易信息:")
+    print(f"从 Legacy 地址: {legacy_address.to_string()}")
+    print(f"从 SegWit 地址: {segwit_address.to_string()}")
+    print(f"从 Taproot 地址: {taproot_address.to_string()}")
+    print(f"到 Legacy 地址: {to_address.to_string()}")
+    print(f"总输入金额: {total_input/100000000} BTC")
+    print(f"发送金额: {amount_to_send/100000000} BTC")
+    print(f"手续费: {fee/100000000} BTC")
+    
+    print("\n您可以在这里广播交易:")
+    print("https://mempool.space/testnet/tx/push")
+    
+    return signed_tx
 
 
-    sig1 = priv1.sign_input(tx, 0, utxos_script_pubkeys[0])
-    sig2 = priv2.sign_segwit_input(tx, 1, script_pubkey2, amounts[1])
-    sig3 = priv3.sign_taproot_input(tx, 2, utxos_script_pubkeys, amounts)
+def estimate_fee(fee_rate=10):
+    base_size = 10
+    legacy_input_size = 148
+    segwit_input_size = 68
+    taproot_input_size = 57
+    output_size = 34
 
-    #set witness sets the witness at a particular input index
-    tx.witnesses = []
+    # 计算总大小
+    total_size = base_size
+    total_size += legacy_input_size + segwit_input_size + taproot_input_size
+    total_size += output_size  # 一个输出
 
-    # tx.witnesses.append(TxWitnessInput([sig2, pub2.to_hex()]))
-    # tx.witnesses.append(TxWitnessInput([sig3]))
-#     tx.set_witness(1, TxWitnessInput([sig2, from_pub.to_hex()]))
+    # 计算手续费（satoshis）
+    fee = total_size * fee_rate
 
-    tx.set_witness(1, TxWitnessInput([sig2, pub2.to_hex()]))
-    txin1.script_sig = Script([sig1, pub1.to_hex()])
-    tx.set_witness(2, TxWitnessInput([sig3]))
-
-
-    # print raw signed transaction ready to be broadcasted
-    print("\nRaw signed transaction:\n" + tx.serialize())
-
-    print("\nTxId:", tx.get_txid())
-    print("\nTxwId:", tx.get_wtxid())
-
-    print("\nSize:", tx.get_size())
-    print("\nvSize:", tx.get_vsize())
-    # print("\nCore vSize:", 160)
-
+    return fee / 100000000  # 转换为BTC
 
 if __name__ == "__main__":
     main()
